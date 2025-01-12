a67a1a3f10249910281d1a9492adbd036d4f479c
Melissa <melisapaksoy@hotmail.com>
2025-01-12 01:15:11 -0500

    add lab1 coding question answers

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -4,6 +4,8 @@
 #Defining variables
 array = [1, 4, 7, 9]
 
+print(array)
+
 #Order of operations
 a=1
 b=2
@@ -12,6 +14,8 @@ d=4
 
 e = ((a - ((b ** c) // d)) + (a % c))
 
+print(f"e = {e}")
+
 #Formatting
 
 temperature = 32.6
@@ -19,5 +23,4 @@ print("The temperature is : {:.3f}  degrees Celsius.".format(temperature))
 
 #Common functions
 userAge = int(input("Please enter your age: "))
-userAge += 22
-print("Now showing the shop items filtered by age: {userAge}")
+print(f"Now showing the shop items filtered by age: {userAge}")
