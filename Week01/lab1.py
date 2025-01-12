13e8666dc2f8b26be05499fd5f1a4660a3e46adc
Melissa <melisapaksoy@hotmail.com>
2025-01-12 00:58:01 -0500

    Add lab1 Coding Questions

---
+++ Week01/lab1.py
@@ -0,0 +1,23 @@
+# Sample Coding Questions 01 Week 01
+# Melissa Paksoy
+
+#Defining variables
+array = [1, 4, 7, 9]
+
+#Order of operations
+a=1
+b=2
+c=3
+d=4
+
+e = ((a - ((b ** c) // d)) + (a % c))
+
+#Formatting
+
+temperature = 32.6
+print("The temperature is : {:.3f}  degrees Celsius.".format(temperature))
+
+#Common functions
+userAge = int(input("Please enter your age: "))
+userAge += 22
+print("Now showing the shop items filtered by age: {userAge}")
