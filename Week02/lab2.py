f21a7a928704aedb6b46fe1c3c6704fc71bf8ff5
Melissa <melisapaksoy@hotmail.com>
2025-01-14 10:48:40 -0500

    import random

--- Week02/lab2.py
+++ Week02/lab2.py
@@ -1,14 +1,15 @@
+import random
+
 elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
 print("Elements: ", elements)
-#git add . && git commit -m "add elements array" && git push
+#  git add . && git commit -m "add elements array" && git push
 
 # def funct_name():
- #   return True
-
- # def say_greeting(name, message="hi"):
- #   print(f" {message}, {name}")
+#   return True
+# def say_greeting(name, message="hi"):
+#   print(f" {message}, {name}")
 # say_greeting("Melissa")
- #   say_greeting("Melissa, Hello")
+# say_greeting("Melissa, Hello")
 
 def get_valid_int_input(prompt):
  while True:
